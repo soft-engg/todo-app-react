164cd5d276d17727e731a0c9e7d515b63a05f024
soft-engg <pshahbaz99@gmail.com>
2022-08-21 18:09:44 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  //array of todos 
   const [todos, setTodos] = useState([])
+  //value of the input field 
   const [value, setvalue] = useState("")
+  //function to add todos
   function createTodo() {
-
     setTodos(oldtodos => {
       setvalue("")
       return [...oldtodos, value]
     })
   }
+  //function to add todos for the enter key pressed 
+  function checkforenterkey(e) {
+    if (e.keyCode === 13)
+      createTodo()
+  }
+
   return (
     <div>
       <h2>Todo app</h2>
-      <input type='text' value={value} onChange={event => {
-        setvalue(event.target.value)
-      }} ></input>
+      <input
+        onKeyDown={checkforenterkey}
+        type='text'
+        value={value}
+        onChange={event => {
+          setvalue(event.target.value)
+        }} />
       <button onClick={createTodo}>Add todo</button>
+      <form></form>
       <ul>
         {todos.map(todo => <li>{todo}</li>)}
       </ul>
